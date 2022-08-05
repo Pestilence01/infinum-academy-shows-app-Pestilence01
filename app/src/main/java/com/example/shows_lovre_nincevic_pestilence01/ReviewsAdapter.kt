86961589fc5c9cb385a5f328ab322332fc361ee6
Pestilence <lovre.nin.zd@gmail.com>
2022-08-05 18:31:00 +0200

    Fixed some merge conflicts

--- app/src/main/java/com/example/shows_lovre_nincevic_pestilence01/ReviewsAdapter.kt
+++ app/src/main/java/com/example/shows_lovre_nincevic_pestilence01/ReviewsAdapter.kt
@@ -1,19 +1,20 @@
-package com.example.shows_lovre_nincevic_pestilence01
+package com.example.shows_lovre_nincevic_pestilence01.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shows_lovre_nincevic_pestilence01.databinding.ReviewItemBinding
+import com.example.shows_lovre_nincevic_pestilence01.models.Review
 
 class ReviewsAdapter(
-    private var items: MutableList<Review>,
+    private var items: ArrayList<Review>,
 ) : RecyclerView.Adapter<ReviewsAdapter.ReviewViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewsAdapter.ReviewViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
         val binding = ReviewItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ReviewViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ReviewsAdapter.ReviewViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
         holder.bind(items[position])
     }
 
